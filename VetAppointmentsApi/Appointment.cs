8b0025e4e7d9b81bf8ff7de2da73a62efd05829d
eryk-urbanski <eryk.urbanski14@gmail.com>
2024-05-31 19:38:33 +0200

    Add the Appointment class

---
+++ VetAppointmentsApi/Appointment.cs
@@ -0,0 +1,9 @@
+public class Appointment
+{
+    public int Id { get; set; }
+    public string? DogName { get; set; }
+    public int DogAge { get; set; }
+    public string? DogColour { get; set; }
+    public string? OwnerName { get; set; }
+    public string? OwnerSurname { get; set; }
+}
