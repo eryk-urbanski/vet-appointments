4168211204a1632c7e71f6166d38abb4aaadefb7
eryk-urbanski <eryk.urbanski14@gmail.com>
2024-05-31 20:27:41 +0200

    Add basic HTTP requests  - GET Get All Apointments  - GET Get Appointment By ID  - POST Create New Appointment  - PUT Update/Modify Appointment By ID  - DELETE Delete Appointment

--- VetAppointmentsApi/Program.cs
+++ VetAppointmentsApi/Program.cs
@@ -26,6 +26,67 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/", () => "Hello World!");
+var AppointmentItems = app.MapGroup("/appointmentitems");
+
+AppointmentItems.MapGet("/", GetAllAppointments);
+AppointmentItems.MapGet("/{id}", GetAppointment);
+AppointmentItems.MapPost("/", CreateAppointment);
+AppointmentItems.MapPut("/{id}", UpdateAppointment);
+AppointmentItems.MapDelete("/{id}", DeleteAppointment);
 
 app.Run();
+
+// GET Get All Appointments
+static async Task<IResult> GetAllAppointments(AppointmentDb db)
+{
+    return TypedResults.Ok(await db.Appointments.ToArrayAsync());
+}
+
+// GET Get Appointment By ID
+static async Task<IResult> GetAppointment(int id, AppointmentDb db)
+{
+    return await db.Appointments.FindAsync(id)
+        is Appointment Appointment
+            ? TypedResults.Ok(Appointment)
+            : TypedResults.NotFound();
+}
+
+// POST Create New Appointment
+static async Task<IResult> CreateAppointment(Appointment Appointment, AppointmentDb db)
+{
+    db.Appointments.Add(Appointment);
+    await db.SaveChangesAsync();
+
+    return TypedResults.Created($"/Appointmentitems/{Appointment.Id}", Appointment);
+}
+
+// PUT Update/Modify Appointment By ID 
+// TODO change to by cat name/owner/smth else?
+static async Task<IResult> UpdateAppointment(int id, Appointment inputAppointment, AppointmentDb db)
+{
+    var Appointment = await db.Appointments.FindAsync(id);
+    if (Appointment is null) return TypedResults.NotFound();
+
+    Appointment.Date = inputAppointment.Date;
+    Appointment.DogName = inputAppointment.DogName;
+    Appointment.DogAge = inputAppointment.DogAge;
+    Appointment.OwnerName = inputAppointment.OwnerName;
+    Appointment.OwnerSurname = inputAppointment.OwnerSurname;
+
+    await db.SaveChangesAsync();
+
+    return TypedResults.NoContent();
+}
+
+// DELETE Delete Appointment
+static async Task<IResult> DeleteAppointment(int id, AppointmentDb db)
+{
+    if (await db.Appointments.FindAsync(id) is Appointment Appointment)
+    {
+        db.Appointments.Remove(Appointment);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+}
